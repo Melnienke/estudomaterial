54d5e2c742f0288d98b39d243e200977297c9e89
Melissa Gomes <melissa.gomes@greenwave.ind>
2023-04-11 12:44:39 -0300

    first commit

---
+++ 5_StringAvancado/scripts.js
@@ -0,0 +1,12 @@
+console.log("Primeira Linha \n segunda linha"); // \n - pula linha 
+
+// imprimir as aspas duplas, insere-se dentro de aspas simples e viceversa
+console.log("Meu nome é 'Mel'");
+console.log('O nome dele é "GustavoCruz"');
+
+// backticks - usado neste caso abaixo para imprimir os valores
+console.log(`A multiplicação de 5 por 3 é ${5 * 3}`);
+
+// ---------- concatenação - somar strings:
+console.log("O " + " meu " + " é " + " Gustavo ");
+
