899cdca660fc72a27c0d5f8e3dea1760df74b82c
Mel <melissabrito2015@outlook.com.br>
2023-04-12 22:50:58 -0300

    atualização

--- Dadosoperadoreslogicos/5_StringAvancado/scripts.js
+++ Dadosoperadoreslogicos/5_StringAvancado/scripts.js
@@ -10,3 +10,23 @@ console.log(`A multiplicação de 5 por 3 é ${5 * 3}`);
 // ---------- concatenação - somar strings:
 console.log("O " + " meu " + " é " + " Gustavo ");
 
+
+
+
+
+// exercicio da matéria: // 
+
+console.log('"Olá, este é o primeiro exercicio"');
+console.log("'Olá, vamos ao exercicio'");
+console.log('O nome dele é gustavo Cruz');
+console.log("Meu nome é Melissa");
+console.log(`Isto é um teste!`);
+
+// exercicio com numeros // 
+
+console.log(123);
+console.log(1,2);
+console.log(1*3);
+
+
+
--- Dadosoperadoreslogicos/7_Boolean comparacao/scripts.js
+++ Dadosoperadoreslogicos/7_Boolean comparacao/scripts.js
@@ -7,3 +7,12 @@ console.log('Gustavo' != 'Gustavo');
 console.log(3 === '3');
 console.log(4 === 4);
 
+
+
+// exercicio com boolean // 
+
+console.log(50 > 60);
+console.log(20 < 30);
+console.log(2 == 2);
+console.log(1 !== 3);
+
